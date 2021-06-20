b6c35c1e57255f4a8d8f069b36489f47055d79bd
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2021-06-21 00:19:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -353,7 +353,8 @@ class Client(discord.Client):
                         name="Hecho por Hat Kid", icon_url="https://cdn.discordapp.com/avatars/363594127885074434/0778dfb946e90e6d645e09fa93d5b247.png?size=2048")
                     embed.set_thumbnail(
                         url="https://cdn.discordapp.com/app-icons/855802712653561876/dd525a11fda30c28c755b636ddf76986.png")
-                    embed.add_field(name="**Comandos:**", value="```TS.setchannel             -  Establecerá en canal donde se ejecute como canal de noticias.\nTS.update                 -  Hará un refresco manual de las noticias de la web.\nTS.ReloadTime [minutos]   -  Establecerá el intervalo de minutos entre los refrescos automáticos.\nTS.help/info              -  Mostrará este mensaje.\nTS.SetPrefix [prefijo]    -  Establecerá el prefijo para los comandos del bot.```".replace("TS.", self.config.getprefix()), inline=True)
+                    embed.add_field(name="**Comandos:**", value="```TS.setchannel             -  Establecerá en canal donde se ejecute como canal de noticias.\nTS.update                 -  Hará un refresco manual de las noticias de la web.\nTS.ReloadTime [minutos]   -  Establecerá el intervalo de minutos entre los refrescos automáticos.\nTS.help/info              -  Mostrará este mensaje.\nTS.SetPrefix [prefijo]    -  Establecerá el prefijo para los comandos del bot.```".replace(
+                        "TS.", self.config.getprefix()), inline=True)
                     embed.set_footer(text="Versión: " + VERSION)
                     await channel.send(embed=embed)
                 elif args[0] == "SetPrefix":
@@ -366,7 +367,7 @@ class Client(discord.Client):
                             embed.set_thumbnail(
                                 url="https://cdn.discordapp.com/app-icons/855802712653561876/dd525a11fda30c28c755b636ddf76986.png")
                             embed.add_field(
-                                name="**Mensaje:**", value="Se a cambiado el prefijo correctamente por `" + self.config.getprefix() +"`", inline=True)
+                                name="**Mensaje:**", value="Se a cambiado el prefijo correctamente por `" + self.config.getprefix() + "`", inline=True)
                             await channel.send(embed=embed)
                         except:
                             channel = message.channel
