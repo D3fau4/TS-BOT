3dff4e63dd9ae65f1b318a36743fbd88013bce8e
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2021-11-28 11:44:06 +0100

    Update

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ config = Config()
 bot = Bot(command_prefix="!", self_bot=True, intents=Intents.default())
 slash = SlashCommand(bot, sync_commands=True, debug_guild=817023577496748052)
 
-bot.load_extension("TS")
+bot.load_extension("Towa")
 bot.run(config.getapiKey())
